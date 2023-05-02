45e5f364159fcd7fdc587252c369ca5d0aefae48
Daniel Kipp <daniel.kipp@gmail.com>
2023-05-02 17:14:45 +0200

    added reference

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_02_150135) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_02_151356) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_02_150135) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "references", force: :cascade do |t|
+    t.string "text"
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_references_on_item_id"
+  end
+
   add_foreign_key "items", "artists"
   add_foreign_key "items", "manufacturers"
+  add_foreign_key "references", "items"
 end
