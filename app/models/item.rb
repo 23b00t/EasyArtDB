a7b9867affe44d71975c1c5d47d99931c9e62748
Daniel Kipp <daniel.kipp@gmail.com>
2023-05-02 17:54:13 +0200

    prefill categories to selection

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,7 @@ class Item < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :references, dependent: :destroy
   has_many :tasks, dependent: :destroy
+
+  CATEGORIES = ['Halskette', "Armband-/ reif", 'Fingerring', 'Brosche', 'Anhänger', 'Sonstiges']
+  validates :category, inclusion: { in: CATEGORIES }
 end
