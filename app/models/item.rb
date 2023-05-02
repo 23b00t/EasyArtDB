7ae7841c760ad606567051e6d9ae6d58332d1bfa
Daniel Kipp <daniel.kipp@gmail.com>
2023-05-02 17:41:31 +0200

    added dependend destroy

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :artist
   belongs_to :manufacturer
-  has_many :comments, :references, :tasks
+  has_many :comments, dependent: :destroy
+  has_many :references, dependent: :destroy
+  has_many :tasks, dependent: :destroy
 end
