c17f3f8d3e5d64760a3ce770076650f6e4641850
Daniel Kipp <daniel.kipp@gmail.com>
2023-05-02 16:26:29 +0200

    created Manufacturer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_02_142322) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_02_142551) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_02_142322) do
     t.date "birthday"
     t.date "deathday"
     t.integer "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "manufacturers", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
