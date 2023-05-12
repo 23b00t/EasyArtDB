e9fde461fce44da99028aff4a667c57b81dd9f77
Daniel Kipp <daniel.kipp@gmail.com>
2023-05-12 13:57:08 +0200

    added ajax functionality

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,11 @@ class ItemsController < ApplicationController
                  .paginate(page: params[:page], per_page: 20)
     @items = @items.where(artist_id: params[:artist_id]) if params[:artist_id].present?
     @items = @items.global_search(params[:query]) if params[:query].present?
+
+    respond_to do |format|
+      format.html
+      format.text { render partial: "items/table", formats: [:html] }
+    end
   end
 
   def show; end
