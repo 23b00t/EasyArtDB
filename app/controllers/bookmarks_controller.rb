9464156bb6fb60efe9a46a63ecad3f4afc35e2ff
Daniel Kipp <daniel.kipp@gmail.com>
2023-05-30 17:21:39 +0200

    working on views

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,26 @@
 class BookmarksController < ApplicationController
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list_id = @list.id
+
+    if @bookmark.save
+      redirect_to list_path(@bookmark.list)
+    else
+      # @show_modal = true
+      # render 'lists/show', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list_id), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:list_id, :movie_id)
+  end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,33 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: %i[show destroy]
+
+  def index
+    @lists = List.all
+  end
+
+  def show; end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save ? (redirect_to list_path(@list)) : (render :new, status: :unprocessable_entity)
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
+  end
 end
