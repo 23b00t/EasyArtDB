bb42a0f976250a8de785681752e91795d4ff8d07
Daniel Kipp <daniel.kipp@gmail.com>
2023-10-06 16:04:31 +0200

    frontend changes to make list selectable and js changes to process data. still need to fix to gather in bookmark_controller.js the item_ids and to process them correctly in bookmarks_controller.rb

--- app/javascript/controllers/bookmark_controller.js
+++ app/javascript/controllers/bookmark_controller.js
@@ -1,11 +1,39 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="bookmark"
 export default class extends Controller {
-  static targets = ["checkboxes"]
+  static targets = ["checkboxes", "listSelect"]
 
-  connect() {
-    console.log("bookmark")
-    console.log(this.checkboxesTargets)
+  createBookmark() {
+    const selectedItems = this.checkboxesTargets.filter(checkbox => checkbox.checked);
+    const selectedListId = this.listSelectTarget.value;
+
+    console.log(selectedItems)
+    console.log(selectedListId)
+
+    if (selectedItems.length > 0 && selectedListId) {
+      const itemIds = selectedItems.map(checkbox => checkbox.value);
+      const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
+
+      fetch('/lists/' + selectedListId + '/bookmarks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRF-Token': csrfToken
+        },
+        body: JSON.stringify({
+          item_ids: itemIds,
+          list_id: selectedListId
+        })
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log("Bookmark erstellt", data);
+        // Hier kannst du auf die Antwort des Servers reagieren, wenn das Bookmark erfolgreich erstellt wurde.
+      })
+      .catch(error => {
+        console.error("Fehler beim Erstellen des Bookmarks", error);
+        // Hier kannst du auf einen Fehler reagieren, wenn das Bookmark nicht erstellt werden konnte.
+      });
+    }
   }
 }
