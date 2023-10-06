849de3cc4c3e346be5f956ee2c48e0c87ea16a40
Daniel Kipp <daniel.kipp@gmail.com>
2023-10-06 17:20:06 +0200

    creating bookmarks works, but the json logic in bookmarks_controller doesnt make sense and it is not displayed correct that the bookmarks was created

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,15 +3,19 @@ require "pry-byebug"
 class BookmarksController < ApplicationController
   def create
     @list = List.find(params[:list_id])
-    @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.list_id = @list.id
+    # @bookmark = Bookmark.new(bookmark_params)
+    # @bookmark.list_id = @list.id
+    @item_ids = params[:item_ids].map(&:to_i)
+    # binding.pry
 
-    binding.pry
+    @item_ids.each do |item|
+      @bookmark = Bookmark.new(item_id: item, list_id: @list.id)
 
-    if @bookmark.save
-      render json: { success: true, bookmark_id: @bookmark.id }
-    else
-      render json: { success: false, errors: @bookmark.errors.full_messages }, status: :unprocessable_entity
+      if @bookmark.save
+        render json: { success: true, bookmark_id: @bookmark.id }
+      else
+        render json: { success: false, errors: @bookmark.errors.full_messages }, status: :unprocessable_entity
+      end
     end
   end
 
@@ -24,6 +28,6 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:list_id, :item_id)
+    params.require(:bookmark).permit(:list_id, :item_ids)
   end
 end
