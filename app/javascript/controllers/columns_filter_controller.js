8cc2c9444b6173c6a3f48411bb415e60530ac879
Daniel Kipp <daniel.kipp@gmail.com>
2023-12-08 17:27:55 +0100

    consolidated bookmark_controller and columns_filter_controller; made style changes to this two functionalities

--- app/javascript/controllers/columns_filter_controller.js
+++ app/javascript/controllers/columns_filter_controller.js
@@ -1,11 +1,9 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="columns-filter"
 export default class extends Controller {
-  static targets = ["toggle", "form", "input", "list", "checkboxes", "foto", "category", "titel", "size", "artist", "manufacturer", "material", "year", "edition", "provenance", "comments", "references", "tasks"]
+  static targets = ["toggle", "form", "input", "list", "listSelect", "itemCheckboxes", "bookmarkForm", "checkboxes", "foto", "category", "titel", "size", "artist", "manufacturer", "material", "year", "edition", "provenance", "comments", "references", "tasks"];
 
   initialize() {
-    // Add event listener to all checkboxes
     this.checkboxesTargets.forEach((checkbox) => {
       checkbox.addEventListener("change", this.checkboxChanged.bind(this))
     })
@@ -21,17 +19,23 @@ export default class extends Controller {
   }
 
   update() {
-    const artistId = document.querySelector("#artist_id").value;
-    const openTasks = document.querySelector("#open_tasks").value;
-    const incomplete = document.querySelector("#incomplete").value;
-
+    const artistId = this.getInputElementValue("#artist_id");
+    const openTasks = this.getInputElementValue("#open_tasks");
+    const incomplete = this.getInputElementValue("#incomplete");
     const url = `${this.formTarget.action}?artist_id=${artistId}&query=${this.inputTarget.value}&open_tasks=${openTasks}&incomplete=${incomplete}&commit=Filter`;
+    this.fetchAndUpdateList(url);
+  }
 
-    fetch(url, {headers: {"Accept": "text/plain"}})
+  fetchAndUpdateList(url) {
+    fetch(url, { headers: { "Accept": "text/plain" } })
       .then(response => response.text())
       .then((data) => {
-        this.listTarget.outerHTML = data
-      })
+        this.listTarget.outerHTML = data;
+      });
+  }
+
+  getInputElementValue(selector) {
+    return document.querySelector(selector).value;
   }
 
   resetForm() {
@@ -43,6 +47,84 @@ export default class extends Controller {
   }
 
   displayForm() {
-    this.toggleTarget.classList.toggle("d-none")
+    this.toggleTarget.classList.toggle("d-none");
+  }
+
+  uncheckAllCheckboxes() {
+    this.itemCheckboxesTargets.forEach((checkbox) => {
+      checkbox.checked = false;
+    });
+  }
+
+  toggleBookmarkForm() {
+    this.bookmarkFormTarget.classList.toggle("d-none");
+  }
+
+  createBookmark() {
+    const selectedItems = this.itemCheckboxesTargets.filter(checkbox => checkbox.checked);
+    const selectedListId = this.listSelectTarget.value;
+
+    if (selectedItems.length > 0 && selectedListId) {
+      const itemIds = selectedItems.map(checkbox => checkbox.name);
+      this.handleBookmarkAction('/lists/' + selectedListId + '/bookmarks', itemIds);
+    }
+  }
+
+  deleteBookmarks() {
+    const selectedItems = this.itemCheckboxesTargets.filter(checkbox => checkbox.checked);
+    const selectedListId = this.extractListIdFromUrl();
+
+    if (selectedItems.length > 0 && selectedListId) {
+      const itemIds = selectedItems.map(checkbox => checkbox.name);
+      this.handleBookmarkAction('/lists/' + selectedListId + '/bookmarks', itemIds);
+    }
+  }
+
+  handleBookmarkAction(url, itemIds) {
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': csrfToken
+      },
+      body: JSON.stringify({
+        item_ids: itemIds,
+        list_id: this.listSelectTarget.value
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        alert("Erfolgreich zur Liste hinzugefügt");
+        setTimeout(() => {
+          this.uncheckAllCheckboxes();
+        }, 100);
+      })
+      .catch(error => {
+        // console.error("Error in Bookmarks action", error);
+        alert("Ups, etwas ist schief gelaufen:", error);
+        setTimeout(() => {
+          this.uncheckAllCheckboxes();
+        }, 100);
+      });
+  }
+
+  extractListIdFromUrl() {
+    const match = window.location.pathname.match(/\/lists\/(\d+)/);
+    return match ? match[1] : null;
+  }
+
+  removeDeletedElements(deletedItemIds) {
+    deletedItemIds.forEach(itemId => {
+      const elementToRemove = document.querySelector(`[data-bookmark-target="itemCheckboxes"][name="${itemId}"]`);
+      if (elementToRemove) {
+        const row = elementToRemove.closest('tr');
+        if (row) {
+          row.remove();
+        } else {
+          elementToRemove.remove();
+        }
+      }
+    });
   }
 }
