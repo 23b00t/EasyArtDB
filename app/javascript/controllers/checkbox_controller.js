2d98de9d0bf8656b22012a15c7f467cc1a0cc64a
Daniel Kipp <daniel.kipp@gmail.com>
2023-12-14 17:23:37 +0100

    cleaned checkbox controller

--- app/javascript/controllers/checkbox_controller.js
+++ app/javascript/controllers/checkbox_controller.js
@@ -1,15 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="checkbox"
+// Controlls checkboxes in tasks/show
 export default class extends Controller {
   static targets = ["checkboxLabel"]
 
-  connect() {
-    console.log('connected')
-  }
-
   toggle(event) {
-    console.log("Toggle method called")
     const checkbox = event.target
     const checked = checkbox.checked
     const label = this.checkboxLabelTarget
