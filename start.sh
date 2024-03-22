ecbefc78a70ddd5fd5336e2cefee128e269baac1
Daniel Kipp <daniel.kipp@gmail.com>
2024-03-22 12:21:21 +0100

    added start script for mac os and improved description of restoring db in frontend

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# run app on mac os
+
+limactl start &
+sleep 5
+export DOCKER_HOST=$(limactl list docker --format 'unix://{{.Dir}}/sock/docker.sock')
+
+docker-compose up -d
+
+google-chrome --incognito --disable-pgu --app=http://localhost:3000
+
+docker-compose down
+
+limactl stop
