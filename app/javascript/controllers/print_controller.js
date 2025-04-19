f459d80390c466f5c5166f77a8fecdc2333fbc5a
Daniel Kipp <daniel.kipp@gmail.com>
2025-04-19 11:12:36 +0200

    Feature: Remmove and restore links for print view; Change app title

--- app/javascript/controllers/print_controller.js
+++ app/javascript/controllers/print_controller.js
@@ -1,15 +1,25 @@
-// app/javascript/controllers/print_controller.js
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["table"];
 
   print() {
-    const table = this.tableTarget;
+    if (this.hasTableTarget) {
+      // Find all links in the table
+      const links = this.tableTarget.querySelectorAll('a');
+      links.forEach(link => {
+        // remove the href attribute
+        link.removeAttribute('href');
+      });
 
-    if (table) {
-      table.style.display = "block";
+      // Show print dialog
+      this.tableTarget.style.display = "block";
       window.print();
+
+      // Restore links
+      links.forEach(link => {
+        link.setAttribute('href', link.getAttribute('data-href'));
+      });
     }
   }
 }
